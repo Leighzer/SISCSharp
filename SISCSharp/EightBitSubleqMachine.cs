d9ef7e5b2c282168962d530e17a48789f459be4f
Leighzer <bwc_nothgiel@live.com>
2021-05-24 00:23:48 -0400

    Fix file org; Progress on machine imp; Start of args intake & validation;

---
+++ SISCSharp/EightBitSubleqMachine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SISCSharp
+{
+    public class EightBitSubleqMachine
+    {
+        public sbyte[] Memory { get; set; }
+
+        public byte a = 0; // address of val to subtract from Memory[b]
+        public byte b = 0; // address to store result of subtraction
+        public byte c = 0; // address to branch to if result <= 0
+
+        public byte ip = 0; // instruction pointer, points to the next subleq instruction run
+
+        public EightBitSubleqMachine(sbyte[] memory)
+        {
+            this.Memory = memory;
+        }
+
+        public void Run()
+        {   
+            while (ip >= 0)
+            {
+                // load new instruction
+                a = (byte) Memory[ip];
+                b = (byte) Memory[ip + 1];
+                c = (byte) Memory[ip + 2];
+
+                // if invalid memory values
+                // add checks for address values, if special value - take input from console or output to console???
+                if (((sbyte) a) < 0 || ((sbyte) b) < 0)
+                {
+                    // set ip to a halting value
+                    ip = unchecked((byte) -1);
+                }
+                else
+                {
+                    SUBLEQ();
+                }
+            }
+        }
+
+        public void SUBLEQ()
+        {   
+            Memory[b] = (sbyte)(Memory[b] - Memory[a]);
+            
+            if (Memory[b] > 0)
+            {
+                ip += 3;
+            }
+            else
+            {
+                ip = c;
+            }
+        }
+    }
+}
---
+++ SISCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SISCSharp
+{
+    public class Program
+    {
+        // needed args
+        // input file/program to run
+        // output file (write tape to)
+        public static void Main(string[] args)
+        {
+            List<string> largs = new List<string>(args);
+
+            if (largs.Count < 2)
+            {
+                Console.WriteLine($"{largs.Count} arguments received. An input file and output file must be provided");
+            }
+
+            string inputFile = largs[0];
+            string outputFile = largs[1];
+            bool overwriteExistingOutputFile = (largs[2] ?? "").ToLower() == "-o";
+
+            // validate input file - make sure exists and we can read it
+
+            // valid output file - if exists ask user if sure you want to overwrite (unless override arg is provided)
+
+            // load subleq machine with input file
+
+            // run subleq machine
+
+            // write subleq machine tap to file
+
+            // done
+        }
+
+        public static string Pad(byte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+
+        // not super helpful since isn't toString for sbyte??
+        public static string Pad(sbyte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+    }
+}
