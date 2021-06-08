3918e472aef6c2f9102d8056c45c4ad1dd51b175
Leighzer <bwc_nothgiel@live.com>
2021-06-07 21:01:16 -0400

    Remove output file arg in preference to redirection for outputting program results; Remove experiments file and byte ToString() helpers;

--- SISCSharp/EightBitSubleqMachine.cs
+++ SISCSharp/EightBitSubleqMachine.cs
@@ -7,17 +7,20 @@ namespace SISCSharp
     // requirements based in part by https://rosettacode.org/wiki/Subleq
     public class EightBitSubleqMachine
     {
-        public sbyte[] Memory { get; set; }
+        private sbyte[] memory { get; set; } = new sbyte[256];
 
-        public byte a = 0; // address of val to subtract from Memory[b]
-        public byte b = 0; // address to store result of subtraction
-        public byte c = 0; // address to branch to if result <= 0
+        private byte a = 0; // address of val to subtract from memory[b]
+        private byte b = 0; // address to store result of subtraction
+        private byte c = 0; // address to branch to if result <= 0
 
-        public byte ip = 0; // instruction pointer, points to the next subleq instruction run
+        private byte ip = 0; // instruction pointer, points to the next subleq instruction run
 
         public EightBitSubleqMachine(sbyte[] memory)
         {
-            this.Memory = memory;
+            for(int i = 0; i < memory.Length && i < 256; i++)
+            {
+                this.memory[i] = memory[i];
+            }
         }
 
         public void Run()
@@ -25,20 +28,20 @@ namespace SISCSharp
             while (((sbyte)ip) != -1)
             {
                 // load new instruction
-                a = (byte) Memory[ip];
-                b = (byte) Memory[ip + 1];
-                c = (byte) Memory[ip + 2];
+                a = (byte) memory[ip];
+                b = (byte) memory[ip + 1];
+                c = (byte) memory[ip + 2];
 
                 if ((sbyte)a == -1)
                 {
                     // read byte
-                    Memory[b] = ((sbyte) Console.ReadKey().KeyChar);
+                    memory[b] = ((sbyte) Console.ReadKey().KeyChar);
                     ip += 3;
                 }
                 else if ((sbyte)b == -1)
                 {
                     // write byte to output
-                    Console.Write((char)Memory[a]);
+                    Console.Write((char)memory[a]);
                     ip += 3;
                 }
                 else
@@ -48,11 +51,11 @@ namespace SISCSharp
             }
         }
 
-        public void SUBLEQ()
-        {   
-            Memory[b] = (sbyte)(Memory[b] - Memory[a]);
+        private void SUBLEQ()
+        {
+            memory[b] = (sbyte)(memory[b] - memory[a]);
             
-            if (Memory[b] > 0)
+            if (memory[b] > 0)
             {
                 ip += 3;
             }
--- SISCSharp/Program.cs
+++ SISCSharp/Program.cs
@@ -6,37 +6,24 @@ using System.IO;
 namespace SISCSharp
 {
     public class Program
-    {
-        // needed args
-        // input file/program to run
-        // output file (write tape to)
+    {   
         public static void Main(string[] args)
         {
             List<string> largs = new List<string>(args);
 
-            if (largs.Count < 2)
+            if (largs.Count < 1)
             {
-                Console.WriteLine($"{largs.Count} arguments received. An input file and output file must be provided");
+                Console.WriteLine($"{largs.Count} arguments received. An input file must be provided");
+                return;
             }
 
             string inputFilePath = largs[0];
-            string outputFilePath = largs[1];
-
-            bool overwriteExistingOutputFile = largs.Any(x => x == "-o");
-            bool outputToConsole = largs.Any(x => x == "-c");
 
             // arg validation
             bool inputFileExists = File.Exists(inputFilePath);
             if (!inputFileExists)
             {
                 Console.WriteLine("Input file does not exist.");
-                return;
-            }
-
-            bool outputFileExists = File.Exists(outputFilePath);
-            if (outputFileExists && !overwriteExistingOutputFile)
-            {
-                Console.WriteLine("Output file already exists. Use arg flag -o to have this file overwritten.");
                 return;
             }
 
@@ -50,17 +37,6 @@ namespace SISCSharp
             EightBitSubleqMachine subleqMachine = new EightBitSubleqMachine(signedMemoryToLoad);
 
             subleqMachine.Run();
-        }
-
-        public static string Pad(byte b)
-        {
-            return Convert.ToString(b, 2).PadLeft(8, '0');
-        }
-
-        // not super helpful since isn't toString for sbyte??
-        public static string Pad(sbyte b)
-        {
-            return Convert.ToString(b, 2).PadLeft(8, '0');
         }
     }
 }
