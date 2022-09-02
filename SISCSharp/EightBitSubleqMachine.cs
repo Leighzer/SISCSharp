2d3fb1cebb7c7186e6899b87548e6007cdc560be
Leighzer <bwc_nothgiel@live.com>
2022-09-01 23:09:46 -0400

    Prefix private fields with _;

--- SISCSharp/EightBitSubleqMachine.cs
+++ SISCSharp/EightBitSubleqMachine.cs
@@ -7,42 +7,42 @@ namespace SISCSharp
     // requirements based in part by https://rosettacode.org/wiki/Subleq
     public class EightBitSubleqMachine
     {
-        private sbyte[] memory { get; set; } = new sbyte[256];
+        private sbyte[] _memory { get; set; } = new sbyte[256];
 
-        private byte a = 0; // address of val to subtract from memory[b]
-        private byte b = 0; // address to store result of subtraction
-        private byte c = 0; // address to branch to if result <= 0
+        private byte _a = 0; // address of val to subtract from memory[b]
+        private byte _b = 0; // address to store result of subtraction
+        private byte _c = 0; // address to branch to if result <= 0
 
-        private byte ip = 0; // instruction pointer, points to the next subleq instruction run
+        private byte _ip = 0; // instruction pointer, points to the next subleq instruction run
 
         public EightBitSubleqMachine(sbyte[] memory)
         {
             for(int i = 0; i < memory.Length && i < 256; i++)
             {
-                this.memory[i] = memory[i];
+                this._memory[i] = memory[i];
             }
         }
 
         public void Run()
         {   
-            while (((sbyte)ip) != -1)
+            while (((sbyte)_ip) != -1)
             {
                 // load new instruction
-                a = (byte) memory[ip];
-                b = (byte) memory[ip + 1];
-                c = (byte) memory[ip + 2];
+                _a = (byte) _memory[_ip];
+                _b = (byte) _memory[_ip + 1];
+                _c = (byte) _memory[_ip + 2];
 
-                if ((sbyte)a == -1)
+                if ((sbyte)_a == -1)
                 {
                     // read byte
-                    memory[b] = ((sbyte) Console.ReadKey().KeyChar);
-                    ip += 3;
+                    _memory[_b] = ((sbyte) Console.ReadKey().KeyChar);
+                    _ip += 3;
                 }
-                else if ((sbyte)b == -1)
+                else if ((sbyte)_b == -1)
                 {
                     // write byte to output
-                    Console.Write((char)memory[a]);
-                    ip += 3;
+                    Console.Write((char)_memory[_a]);
+                    _ip += 3;
                 }
                 else
                 {
@@ -53,15 +53,15 @@ namespace SISCSharp
 
         private void SUBLEQ()
         {
-            memory[b] = (sbyte)(memory[b] - memory[a]);
+            _memory[_b] = (sbyte)(_memory[_b] - _memory[_a]);
             
-            if (memory[b] > 0)
+            if (_memory[_b] > 0)
             {
-                ip += 3;
+                _ip += 3;
             }
             else
             {
-                ip = c;
+                _ip = _c;
             }
         }
     }
